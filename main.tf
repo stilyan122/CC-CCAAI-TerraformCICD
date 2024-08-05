e2c9fb2e783ed4c119364632c0e67c2322d370f0
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-08-05 14:52:58 +0300

    Fixed format for files

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "azurerm" {  
+provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
@@ -34,7 +34,7 @@ resource "azurerm_mssql_database" "database" {
   server_id      = azurerm_mssql_server.server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb = 2
+  max_size_gb    = 2
   sku_name       = "S0"
   zone_redundant = false
 }
@@ -68,15 +68,15 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 
   connection_string {
-    name = "DefaultConnection"
-    type = "SQLAzure"
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
     value = "Data Source=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
 }
 
 resource "azurerm_app_service_source_control" "source_control" {
-  app_id               = azurerm_linux_web_app.web_app.id
-  branch               = "master"
-  repo_url             = var.repo_URL
+  app_id                 = azurerm_linux_web_app.web_app.id
+  branch                 = "master"
+  repo_url               = var.repo_URL
   use_manual_integration = true
 }
--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "Resource group name in Azure"
 }
 
 variable "resource_group_location" {
-  type = string
+  type        = string
   description = "Resource group location in Azure"
 }
 
 variable "app_service_plan_name" {
-  type = string
+  type        = string
   description = "App service plan name"
 }
 
 variable "app_service_name" {
-  type = string
+  type        = string
   description = "App service name in Azure"
 }
 
 variable "sql_server_name" {
-  type = string
+  type        = string
   description = "SQL server name in Azure"
 }
 
 variable "sql_database_name" {
-  type = string
+  type        = string
   description = "SQL database name in Azure"
 }
 
 variable "sql_admin_login" {
-  type = string
+  type        = string
   description = "SQL admin username"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type        = string
   description = "SQL admin password"
 }
 
 variable "firewall_rule_name" {
-  type = string
+  type        = string
   description = "Firewall rule name"
 }
 
 variable "repo_URL" {
-  type = string
+  type        = string
   description = "Repository URL in GH"
 }
