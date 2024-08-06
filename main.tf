dbc9c74e104ee63a39ad74339b42bf1f1cb71d5a
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-08-06 14:36:39 +0300

    Fully implemented CI & CD with Azure, Terraform and GA - fixed files and made better architecture

--- main.tf
+++ main.tf
@@ -1,25 +1,19 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.114.0"
-    }
-  }
-}
-
+# Providers
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
 
+provider "azuread" {
+  version = "~> 2.0"
+}
+
+# Resource Group for Terraform State
 resource "azurerm_resource_group" "storage_rg" {
   name     = var.storage_resource_group_name
   location = var.storage_resource_group_location
 }
 
+# Storage Account
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.storage_rg.name
@@ -28,31 +22,44 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = "LRS"
 }
 
+# Storage Container for State
 resource "azurerm_storage_container" "state_container" {
   name                  = var.state_container_name
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
 
+# Azure AD Application
 resource "azuread_application" "app" {
   display_name = var.service_principal_app_name
 }
 
+# Azure AD Service Principal
 resource "azuread_service_principal" "sp" {
-  application_id = azuread_application.app.application_id
+  application_id = azuread_application.app.app_id
 }
 
+# Azure AD Service Principal Password
+resource "azuread_service_principal_password" "sp_password" {
+  service_principal_id = azuread_service_principal.sp.id
+  value                = var.sp_password
+  end_date             = "2099-01-01T00:00:00Z"
+}
+
+# Role Assignment
 resource "azurerm_role_assignment" "sp_role" {
   principal_id   = azuread_service_principal.sp.id
   role_definition_name = "Contributor"
   scope          = azurerm_resource_group.storage_rg.id
 }
 
+# Resource Group for Application Deployment
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
+# SQL Server
 resource "azurerm_mssql_server" "server" {
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.resource_group.name
@@ -62,6 +69,7 @@ resource "azurerm_mssql_server" "server" {
   administrator_login_password = var.sql_admin_password
 }
 
+# SQL Database
 resource "azurerm_mssql_database" "database" {
   name           = var.sql_database_name
   server_id      = azurerm_mssql_server.server.id
@@ -72,6 +80,7 @@ resource "azurerm_mssql_database" "database" {
   zone_redundant = false
 }
 
+# SQL Firewall Rule
 resource "azurerm_mssql_firewall_rule" "firewall_rule" {
   name             = var.firewall_rule_name
   server_id        = azurerm_mssql_server.server.id
@@ -79,6 +88,7 @@ resource "azurerm_mssql_firewall_rule" "firewall_rule" {
   end_ip_address   = "0.0.0.0"
 }
 
+# App Service Plan
 resource "azurerm_service_plan" "service_plan" {
   name                = var.app_service_plan_name
   resource_group_name = azurerm_resource_group.resource_group.name
@@ -87,6 +97,7 @@ resource "azurerm_service_plan" "service_plan" {
   sku_name            = "F1"
 }
 
+# Web App
 resource "azurerm_linux_web_app" "web_app" {
   name                = var.app_service_name
   resource_group_name = azurerm_resource_group.resource_group.name
@@ -107,6 +118,7 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 }
 
+# App Service Source Control
 resource "azurerm_app_service_source_control" "source_control" {
   app_id                 = azurerm_linux_web_app.web_app.id
   branch                 = "master"
